6a24dfb5b24abf620643b8f374f3871a59cb1d55
Final Gitla <finishtest132@gmail.com>
2023-06-15 22:16:09 +0200

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,11 +1,14 @@
-const getCroppedImageUrl = (url: string) => {
-
-    if (!url) return ''
+import noImage from "../assets/noImage.png"
 
 
-    const target = 'media/'
-    const index = url.indexOf(target) + target.length 
-    return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
-};
+function getCroppedImageUrl(url: string) {
+
+    if (!url) return noImage;
+
+
+    const target = 'media/';
+    const index = url.indexOf(target) + target.length;
+    return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+}
 
 export default getCroppedImageUrl;
