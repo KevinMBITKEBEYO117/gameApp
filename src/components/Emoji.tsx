93ac3ce4a8663e2adfa032c46f0e87de48472423
Final Gitla <finishtest132@gmail.com>
2023-06-20 17:02:08 +0200

    Add emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+
+  const emojiMap: { [key: number]: any } = {
+    3: { src: "mey", alt: "good" },
+    4: { src: "ThumbsUp", alt: "recommended" },
+    5: { src: "bullEyes", alt: "exceptional" },
+  };
+
+  return <div {...emojiMap[rating]} ></div>;
+};
+
+export default Emoji;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -11,16 +12,18 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={ getCroppedImageUrl(game.background_image)} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack justifyContent={"space-between"} marginBottom={3}>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
-        <CriticScore score={game.metacritic} />
-
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize={"2xl"}> {game.name} </Heading>
+        <Heading fontSize={"2xl"}>
+          {" "}
+          {game.name} <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   );
