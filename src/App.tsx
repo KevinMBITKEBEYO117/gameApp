347cc320e5e422dec0cc9b360ec3e225c9b1e645
Kevin Mbitkebeyo <mbitkebeyokevin@gmail.com>
2023-12-01 06:04:50 -0500

    Refactor - simplify game query

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,12 @@ import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 import GameGrid from "./components/GameGrid";
 
+//undefined: the absence of a value
+// null: the intentional absence of a value
+
 export interface GameQuery {
-  genre: Genre | null;
-  platform: Platform | null;
+  genreId?: number;
+  platformId?: number
   sortOrder: string;
   searchText: string;
 }
@@ -41,8 +44,8 @@ function App() {
         <Show above="lg">
           <GridItem area="aside" paddingX={5}>
             <GenreList
-              selectedGenre={gameQuery.genre}
-              onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+              selectedGenreId={gameQuery.genreId}
+              onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genreId:genre.id })}
             />
           </GridItem>
         </Show>
@@ -52,9 +55,9 @@ function App() {
           <Flex marginBottom={5}>
             <Box marginRight={5}>
               <PlatformSelector
-                selectedPlatform={gameQuery.platform}
+                selectedPlatformId={gameQuery.platformId}
                 onSelectedPlatform={(platform) =>
-                  setGameQuery({ ...gameQuery, platform })
+                  setGameQuery({ ...gameQuery, platformId:platform.id })
                 }
               />
             </Box>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,10 +4,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props{
   onSelectGenre: (genre:Genre) => void;
-  selectedGenre: Genre|null
+  selectedGenreId?: number
 }
 
-const GenreList = ({onSelectGenre, selectedGenre}:Props) => {
+const GenreList = ({onSelectGenre, selectedGenreId}:Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null
@@ -25,7 +25,7 @@ const GenreList = ({onSelectGenre, selectedGenre}:Props) => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Button whiteSpace='normal' textAlign={"left"} fontWeight={genre.id === selectedGenre?.id ? 'bold':'normal'} onClick={()=> onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+            <Button whiteSpace='normal' textAlign={"left"} fontWeight={genre.id === selectedGenreId ? 'bold':'normal'} onClick={()=> onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
           </HStack>
         </ListItem>
       ))}
