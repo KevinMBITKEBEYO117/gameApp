8ca3c1c41bec966286cec04d5123c00b8a2f9cb0
Kevin Mbitkebeyo <mbitkebeyokevin@gmail.com>
2023-12-04 06:14:47 -0500

    refacto simplify time calculation

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 
-import  APIClient from "../services/api-client";
+import ms from "ms";
+import APIClient from "../services/api-client";
 
 
 const apiClient = new APIClient<Genre>('/genres')
@@ -19,7 +20,7 @@ export interface Genre {
 const useGenres = ()=>useQuery({
   queryKey: ['genres'],
   queryFn: apiClient.getAll,
-  staleTime: 24 * 60 * 60 * 100,
+  staleTime: ms('24h'),
   initialData: genres
 })
 
